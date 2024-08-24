9e8199613510890c96d958cb75e515360e86f8e0
HARISHKUMAR3003 <119477459+HARISHKUMAR3003@users.noreply.github.com>
2024-08-24 19:22:47 +0530

    script.js

--- script.js
+++ script.js
@@ -1,110 +1,108 @@
-body{
-    margin:auto;
-    text-align: center;
-    background-image:url(https://codetheweb.blog/assets/img/posts/css-advanced-background-images/cover.jpg);
-    height: 100vh;
-    background-size: cover;
-    background-repeat: no-repeat; 
-}
+var player='X'
+var cnt=0;
+var result=""
+Done=true
 
-h1{
-    background-image: linear-gradient(rgb(255, 0, 242), rgb(8, 52, 229));
-    color: white;
-    display: inline-block;
-    padding: 15px;
-    font-family: Arial, Helvetica, sans-serif;
-    border-radius: 20px;
-}
 
-.button{
-    margin-top: 3%;
-    display: grid;
-    grid-template-columns: auto auto  auto;
-    width: 100%;
-    justify-content: center;
-    opacity: 0.6;
-}
-
-button{
-    padding: 0px;
-    cursor: pointer;
-    background-color: black;
-    color : white;
-    width: 120px;
-    height: 120px;  
-    font-size: 200%;
-    border-radius: 10px;
-  
-}
-
-h2
+function play(val)
 {
-    display: inline-block; 
-    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
-    font-size: 200%;
-    color:white;
-    border-radius: 10px;
-    background-image: linear-gradient(rgb(255, 0, 242), rgb(8, 52, 229));
+    document.getElementById('restart').innerText="Restart";
+    document.getElementById('restart').style="padding:10px;";
+    if(Done)
+    {    
+    var b=document.getElementById(val)
+    if(player==='X'  && !b.textContent){
+        b.innerText=player;
+        b.style="background-color:rgb(5, 89, 255)"
+        player='O'
+    }
+    else if(player==='O' && !b.textContent){
+
+        b.innerText=player;
+        b.style="background-color:rgb(255, 0, 183);"
+        player='X'
+    }
+    else{
+        return;
+    }
+
+    if(checkwin())
+    {
+        document.getElementById('res').innerHTML=result;
+        document.getElementById('res').style="padding:10px;";
+        Done=false;
+        return;
+    }
+
+    cnt++;
+    if(cnt===9)
+    {
+        cnt=0;
+        result="it's a tie!"
+        document.getElementById('res').innerHTML=result;
+        document.getElementById('res').style="padding:10px;";
+        return;
+    }
+}
 }
 
-#restart
+
+function checkwin() {
+    const winningCombinations = [
+        ['b1', 'b2', 'b3'],
+        ['b4', 'b5', 'b6'],
+        ['b7', 'b8', 'b9'],
+        ['b1', 'b4', 'b7'],
+        ['b2', 'b5', 'b8'],
+        ['b3', 'b6', 'b9'],
+        ['b1', 'b5', 'b9'],
+        ['b3', 'b5', 'b7']
+    ];
+    for(var i=0;i<winningCombinations.length;i++)
+    {
+        let a=document.getElementById(winningCombinations[i][0])
+        let b=document.getElementById(winningCombinations[i][1])
+        let c=document.getElementById(winningCombinations[i][2])
+                if(a && a.textContent===b.textContent && a.textContent===c.textContent && a.textContent==='X'){
+                    result="'X' won!"
+                    return true;
+                }
+                else if(a && a.textContent===b.textContent && a.textContent===c.textContent && a.textContent==='O')
+                {
+                    result="'O' Won!"
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
+function restart()
 {
-    width: auto;
-    height: auto;
-    font-size: 200%;
-    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
-    border-radius: 30px;
-    background-color: rgb(73, 164, 79);
-    border: none;
-}
-#restart:active {
-    transform: scale(0.9);
-}
-#restart:hover
-{
-    scale: 1.1;
-    background-color: red;
-}
-
-
-@media (max-width: 400px) {
-    button {
-        font-size: 200%;
-        padding: 20px;
-        height: 70px;
-        width: 70px;
-    }
-
-    h1 {
-        font-size: 220%;
-    }
-
-    h2 {
-        font-size: 120%;
-    }
-
-    #restart {
-        font-size: 100%;
-    }
+    document.getElementById('b1').textContent="";
+    document.getElementById('b2').textContent="";
+    document.getElementById('b3').textContent="";
+    document.getElementById('b4').textContent="";
+    document.getElementById('b5').textContent="";
+    document.getElementById('b6').textContent="";
+    document.getElementById('b7').textContent="";
+    document.getElementById('b8').textContent="";
+    document.getElementById('b9').textContent="";
+    document.getElementById('res').textContent="";
+    document.getElementById('res').style="padding:0px;";
+    document.getElementById('restart').innerText="";
+    document.getElementById('restart').style="padding:0px;";
+    document.getElementById('b1').style="background-color:black;";
+    document.getElementById('b3').style="background-color:black;";
+    document.getElementById('b2').style="background-color:black;";
+    document.getElementById('b4').style="background-color:black;";
+    document.getElementById('b5').style="background-color:black;";
+    document.getElementById('b6').style="background-color:black;";
+    document.getElementById('b7').style="background-color:black;";
+    document.getElementById('b8').style="background-color:black;";
+    document.getElementById('b9').style="background-color:black;";
+    Done=true;
+    cnt=0;
 }
 
-@media (min-width: 1800px) {
-    button {
-        font-size: 400%;
-        height: 200px;
-        width: 200px;
-    }
 
-    h1 {
-        font-size: 400%;
-    }
-
-    h2 {
-        font-size: 400%;
-    }
-
-    #restart {
-        font-size: 400%;
-        border-radius: 50px;
-    }
-}
